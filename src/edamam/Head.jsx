924bece86ad9b7e2c7b62407f818a29198471197
webdevaadil <75753195+webdevaadil@users.noreply.github.com>
2022-07-11 19:14:57 +0530

    Update Head.jsx

--- src/edamam/Head.jsx
+++ src/edamam/Head.jsx
@@ -14,7 +14,7 @@ export const Head = (props) => {
                         <div className="container">
                             <h1 className="text-center pt-5"> Recipe search APi</h1>
                             <form method='post' className='d-flex mt-5' onSubmit={props.sub}  >
-                                <input type="text" className='form-control ' onChange={props.inputChangeValue}  value={props.inputvalue} />
+                                <input type="text" className='form-control ' onChange={props.inputChangeValue} placeholder="Search here" value={props.inputvalue} />
                                 <input  className='btn btn-primary ml-2'type="submit" />
                             </form>
                         </div>
